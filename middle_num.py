a69333ddb25ed4d06981754503c7da417ea185d0
Deepti-20 <80585331+Deepti-20@users.noreply.github.com>
2021-07-13 09:55:31 +0530

    Add files via upload

---
+++ middle_num.py
@@ -0,0 +1,15 @@
+# list1=[4,3,1]
+# i=0
+# while i<len(list1):
+#     if i==1:
+#         print(list1[i])
+#     i=i+1
+
+
+
+lis=[4,9,1]
+i=-1
+while i>=(-len(lis)):
+    if i==-2:
+        print(lis[i]) 
+    i=i-1
